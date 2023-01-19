9e85edded2b58a7a16ae30da55b6cf8f971f22c4
Andrew <malinaenglishclub@gmail.com>
2023-01-19 21:04:30 +0300

    missing feature added, losing and winning modals added

--- src/Components/Hangman/Hangman.jsx
+++ src/Components/Hangman/Hangman.jsx
@@ -2,38 +2,39 @@ import React from "react";
 import { useState } from "react";
 import { Word } from "./Word";
 import "./hangman.scss";
-let alphabet = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
+export let alphabet = [
+  { letter: "a", flag: false },
+  { letter: "b", flag: false },
+  { letter: "c", flag: false },
+  { letter: "d", flag: false },
+  { letter: "e", flag: false },
+  { letter: "f", flag: false },
+  { letter: "g", flag: false },
+  { letter: "h", flag: false },
+  { letter: "i", flag: false },
+  { letter: "j", flag: false },
+  { letter: "k", flag: false },
+  { letter: "l", flag: false },
+  { letter: "m", flag: false },
+  { letter: "n", flag: false },
+  { letter: "o", flag: false },
+  { letter: "p", flag: false },
+  { letter: "q", flag: false },
+  { letter: "r", flag: false },
+  { letter: "s", flag: false },
+  { letter: "t", flag: false },
+  { letter: "u", flag: false },
+  { letter: "v", flag: false },
+  { letter: "w", flag: false },
+  { letter: "x", flag: false },
+  { letter: "y", flag: false },
+  { letter: "z", flag: false },
 ];
 export const Hangman = ({ words, setWords }) => {
   const [game, setGame] = useState(false);
   const [word, setWord] = useState("");
-  const [alpha, setAlphabet] = useState([]);
+  const [alpha, setAlphabet] = useState(alphabet);
+
   let terms = words.map((el) => el.term);
   let randomWord = terms[Math.floor(Math.random() * terms.length)];
   let newWord = word.split("");
@@ -45,12 +46,19 @@ export const Hangman = ({ words, setWords }) => {
           onClick={() => {
             setGame(!game);
             setWord(randomWord);
-            setAlphabet(alphabet);
           }}
         >
           Играть
         </button>
-      )) || <Word newWord={newWord} alpha={alpha} />}
+      )) || (
+        <Word
+          newWord={newWord}
+          alpha={alpha}
+          randomWord={randomWord}
+          setAlphabet={setAlphabet}
+          setWord={setWord}
+        />
+      )}
     </div>
   );
 };
--- src/Components/Hangman/Word.jsx
+++ src/Components/Hangman/Word.jsx
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { alphabet } from "./Hangman";
 
-export const Word = ({ newWord, alpha }) => {
+const miss = [
+  { x: "x", state: false, id: 0 },
+  { x: "x", state: false, id: 1 },
+  { x: "x", state: false, id: 2 },
+  { x: "x", state: false, id: 3 },
+  { x: "x", state: false, id: 4 },
+  { x: "x", state: false, id: 5 },
+  { x: "x", state: false, id: 6 },
+  { x: "x", state: false, id: 7 },
+  { x: "x", state: false, id: 8 },
+  { x: "x", state: false, id: 9 },
+];
+
+export const Word = ({ newWord, alpha, randomWord, setAlphabet, setWord }) => {
+  const [next, setNext] = useState(miss);
+  const [incr, setIncr] = useState(0);
+
+  const [lost, setLost] = useState(false);
+  const [clear, setClear] = useState(false);
+  const [winner, setWinner] = useState(false);
+
   const handleAlphabet = (e) => {
+    setAlphabet(
+      alpha.map((el) =>
+        el.letter === e.currentTarget.innerHTML ? { ...el, flag: true } : el
+      )
+    );
     let cunt = document.querySelectorAll(".hangman__word div span");
+    let foundLetter = [...cunt];
+    let fart = foundLetter.filter((el) => / /.test(el.innerHTML));
+    if (fart.length > 0) {
+      fart.forEach((el) => el.classList.add("fuckU"));
+    }
     if (newWord.includes(e.currentTarget.innerHTML)) {
-      let foundLetter = [...cunt];
       let whore = foundLetter.filter(
         (el) => el.innerHTML === e.currentTarget.innerHTML
       );
+
       whore.forEach((el) => el.classList.add("fuckU"));
     } else {
-      console.log("u r bitch");
+      setNext(next.map((el) => (el.id === incr ? { ...el, state: true } : el)));
+      setIncr(incr + 1);
+      if (incr === 9) {
+        setLost(true);
+      }
     }
-
-    e.currentTarget.classList.add("notBitch");
+    let all = foundLetter.every((el) => el.classList.contains("fuckU"));
+    if (all) {
+      setWinner(true);
+    }
+  };
+  const handleReset = () => {
+    let cunt = document.querySelectorAll(".hangman__word div span");
+    let foundLetter = [...cunt];
+    foundLetter.forEach((el) => el.classList.remove("fuckU"));
+    setClear(!clear);
+    setLost(false);
+    setWinner(false);
+    setNext(
+      next.map((el) => (el.state === true ? { ...el, state: false } : el))
+    );
+    setWord(randomWord);
+    setAlphabet(alphabet);
+    setIncr(0);
   };
   return (
     <>
@@ -32,10 +83,46 @@ export const Word = ({ newWord, alpha }) => {
       </div>
       <div className="hangman__alphabet">
         {alpha.map((el) => (
-          <div key={el} onClick={(e) => handleAlphabet(e)}>
-            {el}
+          <div
+            key={el.letter}
+            style={{
+              backgroundColor:
+                el.flag === true ? "rgba(128, 128, 128, 0.578)" : "bisque",
+            }}
+            onClick={(e) => handleAlphabet(e)}
+          >
+            {el.letter}
           </div>
         ))}
+      </div>
+      <div className="hangman__miss">
+        {next.map((el, index) => (
+          <div
+            style={{ color: el.state === true ? "red" : "white" }}
+            key={index}
+          >
+            {el.x}
+          </div>
+        ))}
+      </div>
+      {lost === true && (
+        <div className="hangman__lostWin">
+          <p>
+            U lost Bitch!!!! this word was <span>{newWord}</span>
+          </p>{" "}
+          <br />
+          <button onClick={handleReset}>новая игра</button>
+        </div>
+      )}
+      {winner === true && (
+        <div className="hangman__lostWin">
+          <p>U won Bitch!!!! Congratulations!</p> <br />
+          <button onClick={handleReset}>новая игра</button>
+        </div>
+      )}
+      <div className="hangman__buttons">
+        <button onClick={handleReset}>новая игра</button>
+        <button>слово целиком</button>
       </div>
     </>
   );
